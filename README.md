b5115031cf922f8037155989510b06802299b68a
haleluque <henry-luque@hotmail.com>
2025-02-26 18:18:35 -0500

    added Swagger ui doc and content negotiation example

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Spring boot project with restful web services examples:
 ## Exercises
 - Basic Hello World RestController example showing rest mapping types and path variables.
 - Basic Social Media RestController CRUD example. With validations, GlobalExceptionHandler and CustomExceptionHandling.
+- REST API Documentation example with Swagger UI and Open API specification. (host/swagger-ui/index.html)
+- Content Negotiation example (Format: JSON/XML, language support)
--- src/main/java/com/haleluque/rest/restful_web_services/socialMedia/controllers/UserController.java
+++ src/main/java/com/haleluque/rest/restful_web_services/socialMedia/controllers/UserController.java
@@ -4,19 +4,24 @@ import com.haleluque.rest.restful_web_services.socialMedia.model.User;
 import com.haleluque.rest.restful_web_services.socialMedia.data.UserDaoService;
 import com.haleluque.rest.restful_web_services.socialMedia.exception.UserNotFoundException;
 import jakarta.validation.Valid;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class UserController {
     private final UserDaoService service;
+    private final MessageSource messageSource;
 
-    public UserController(UserDaoService service) {
+    public UserController(UserDaoService service, MessageSource messageSource) {
         this.service = service;
+        this.messageSource = messageSource;
     }
 
     @GetMapping("/users")
@@ -48,5 +53,11 @@ public class UserController {
     @DeleteMapping("/users/{id}")
     public void deleteUser(@PathVariable int id) {
         service.deleteById(id);
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale );
     }
 }
